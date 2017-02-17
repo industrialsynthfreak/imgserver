0355da4639c107ec274deacc6eb41cfd6fedd9b8
industrialsynthfreak <electrostatix@sigaint.org>
2017-02-17 19:03:39 +0300

    some changes

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
-from bottle import route, run, template
+from bottle import route, run, template, static_file, error, url
 
+
+@route('/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='static')
 
 @route('/')
 @route('/index')
 def index():
-    return template('index.html')
+    return template('index.html', url=url)
+
+
+@error(404)
+def error404(error):
+    return template('404.html', url=url)
 
 
 if __name__ == "__main__":
